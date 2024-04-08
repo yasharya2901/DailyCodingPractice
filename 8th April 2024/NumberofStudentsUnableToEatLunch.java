4d4b49799c3bf5eb22d305473618f350a27ee30a
yasharya2901 <yasharya2901@gmail.com>
2024-04-08 11:14:01 +0530

    8th April 2024 Daily LeetCode question https://leetcode.com/problems/number-of-students-unable-to-eat-lunch

---
+++ 8th April 2024/NumberofStudentsUnableToEatLunch.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+/*
+ * https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+ */
+
+public class NumberofStudentsUnableToEatLunch {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Stack<Integer> sw = new Stack<Integer>();
+        for (int i = sandwiches.length - 1; i >= 0; i--) {
+            sw.push(sandwiches[i]);
+        }
+
+        Queue<Integer> stu = new LinkedList<>();
+        int[] count = new int[2]; 
+        for (int i = 0; i < students.length; i++) {
+            count[students[i]]++;
+            stu.add(students[i]);
+        }
+
+        while (!stu.isEmpty() && !sw.isEmpty()) {
+            if (count[sw.peek()] == 0) {
+                break;
+            }
+            if (stu.peek() == sw.peek()) {
+                int a = stu.poll();
+                sw.pop();
+                count[a]--;
+            } else {
+                stu.add(stu.poll());
+            }
+        }
+
+        return stu.size();
+    }
+}
